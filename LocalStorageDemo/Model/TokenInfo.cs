120ecefdd028111f7b342f25ed67a72f320dafad
Chandradev Sah <chandradev819@gmail.com>
2024-09-14 20:46:08 +0530

    Update code has been updated.

---
+++ LocalStorageDemo/Model/TokenInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace LocalStorageDemo
+{
+    public class TokenInfo
+    {
+        public string Name { get; set; } // Client Token, Shopper Token, etc.
+        public string Value { get; set; } // Token value (string)
+        public DateTime? ExpiryDate { get; set; } // Nullable for tokens without expiry (Account Id, Shopper Id)
+    }
+}
